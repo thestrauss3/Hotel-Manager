ac0f189a4ac27650f31d3f1c01235baa42b7ff1d
Joe <joseph_strauss@yahoo.com>
2017-06-29 19:12:07 -0400

    react components configuration

--- app/assets/javascripts/components/HelloWorld.jsx
+++ app/assets/javascripts/components/HelloWorld.jsx
@@ -1,5 +1,9 @@
 var HelloMessage = React.createClass({
+  getCurrentBookings: function() {
+    debugger;
+  },
   render: function() {
+    debugger;
     return (
       <h1>Hello {this.props.name}!</h1>
     )
@@ -9,5 +13,22 @@ var HelloMessage = React.createClass({
 const TestComponent = props => {
   return (
       <div>constant{props.name}</div>
+  )
+}
+
+const Schedule = props => {
+  for (i = 0; i < window.innerWidth; i += 100) {
+
+  }
+  var bookings = props.bookings.map((booking, index) => {
+    return(
+      <div></div>
+    )
+  })
+  return (
+    <div>
+
+      {bookings}
+    </div>
   )
 }
