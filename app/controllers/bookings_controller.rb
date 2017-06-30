f4b7161a80ffddbdefe688ab8b9d7a7ca68cb796
Joe <joseph_strauss@yahoo.com>
2017-06-29 23:10:24 -0400

    method to get available rooms.... in progress though

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,6 @@
 class BookingsController < ApplicationController
+  def new
+    @customer = Customer.find(params[:customer_id])
+    @booking = Booking.new
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,21 @@
+require 'pry'
 class Room < ApplicationRecord
   validates :room_number, :room_type, :beds, :bed_type, presence: true
 
   belongs_to :hotel
   has_many :bookings
+
+  def is_available(night = DateTime.now.end_of_day)
+    active_bookings = self.bookings.where("checkin_date < ? AND checkout_date > ?", night, night)
+    active_bookings.empty? || !(active_bookings.pluck(:checkout_time).any?{ |e| e.nil? })
+    #no scheduled bookings, or there were scheduled bookings but they DO have a checkout_time i.e. they have already left
+  end
+
+  def self.get_available(night = DateTime.now.end_of_day)
+    available_rooms = []
+    Room.all.each do |room|
+      available_rooms.push room if room.is_available(night)
+    end
+    return available_rooms
+  end
 end
